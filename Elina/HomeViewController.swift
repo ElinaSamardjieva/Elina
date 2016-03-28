afca463076cbc1c205f1a27d9400b4b2a1c4d253
ElinaSamardjieva <elina.sm2@gmail.com>
2016-03-28 18:56:14 +0300

    Toolbar in DatePicker added (buttons not working)

--- Elina/HomeViewController.swift
+++ Elina/HomeViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var invisibleView: UIView!
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var birthdayButton: UIButton!
+    
+    var birthdayPickerView: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: - Scroll view color
         scrollView.backgroundColor = UIColor(red: 0.7, green: 0.9, blue: 1.0, alpha: 1.0)
+        
+        // MARK: - Keyboard dismiss
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        // MARK: - Button UI
+        birthdayButton.layer.borderWidth = 1
+        birthdayButton.layer.borderColor = UIColor.lightGrayColor().CGColor
+        birthdayButton.layer.cornerRadius = 5
+    
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -29,5 +44,59 @@ class HomeViewController: UIViewController {
         profileImageView.layer.borderWidth = 2.0
         profileImageView.layer.borderColor = UIColor.whiteColor().CGColor
 
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: - DatePicker functions
+    
+    @IBAction func birthdayButtonClicked(sender: UIButton) {
+        
+        birthdayPickerView = UIDatePicker()
+        birthdayPickerView.datePickerMode = UIDatePickerMode.Date
+        //sender.inputView = birthdayPickerView
+        birthdayPickerView.addTarget(self, action: Selector("handleDatePicker:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        let pickerSize : CGSize = birthdayPickerView.sizeThatFits(CGSizeZero)
+        birthdayPickerView.frame = CGRectMake(0.0, 250, pickerSize.width, 200)
+        birthdayPickerView.backgroundColor = UIColor.whiteColor()
+        
+        // MARK: - Add toolbar
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("Done_Click"))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("Cancel_Click"))
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        
+        birthdayPickerView.addSubview(toolBar)
+        
+        self.view.addSubview(birthdayPickerView)
+    }
+    
+    func handleDatePicker(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .NoStyle
+        //birthdayButton.setTitle = dateFormatter.stringFromDate(sender.date)
+        birthdayButton.setTitle(dateFormatter.stringFromDate(sender.date), forState: .Normal)
+
+    }
+    
+    func Done_Click()
+    {
+        print("Hello")
+        birthdayPickerView.hidden = true
+    }
+    
+    func Cancel_Click()
+    {
+        print("Hello")
+        birthdayPickerView.backgroundColor = UIColor.yellowColor()
     }
 }
